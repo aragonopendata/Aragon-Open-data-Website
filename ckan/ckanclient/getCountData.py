e67d6b3ee0d2644a3191ea1beb32cb3292342591
eunzue <gederintheghetto@gmail.com>
2015-04-14 10:20:11 +0200

    Añadir script getDataCount
    Se añade un script, que sirve para rellenar las estadísticas que aparecen en la parte de abajo de la web. Hace uso de la api para obtener los datos. De este modo  en lugar de obtener estos datos con cada carga de la página (con su correspondiente retraso de la carga completa de la página) lo obtenemos con la ejecución del dicho script.
    Este script se debería ejecutar cada cierto tiempo.
    El script intermedio con los valores esta en /var/www/wolfcms/public/getDataCount.js

---
+++ ckan/ckanclient/getCountData.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import os
+import sys
+DIR='/var/www/wolfcms/public/'
+#Este script obtiene el número de dataset y recursos haciendo uso de la api y genera un javascript que contiene su valor
+
+#Método que obtiene el numero de dataset y recursos haciendo uso de la api
+#Nombre es el nombre final que tendra el fichero y servidor el servidor donde se hara preopendata o opendata
+def obtenDatos(nombre, servidor):
+	sentenciaCurl = 'curl  \"' + servidor+'/catalogo/api/getDataCount\" > '+ DIR+nombre
+	os.system(sentenciaCurl)
+	with open(DIR+nombre) as data_file:
+		datos = json.load(data_file)
+	os.remove(DIR+nombre)
+	return datos
+#	pprint(data)
+#	print data[0]["datasetCount"]
+
+#método que genera el script con los datos de la variable
+def generaJavascript(datos):
+	fichero=DIR+'getDataCount.js'
+	f = open(fichero, "w")
+#	f.write("<script type=\"text/javascript\">\n")
+	for dato in datos:
+		f.write('\tvar datasetCount = '+str(dato["datasetCount"])+';\n')
+		f.write('\tvar resourceCount = '+str(dato["resourceCount"])+';\n')
+#	f.write("</script>\n")
+	f.close()
+	
+def main():
+	if (len(sys.argv) == 2):
+		if (str(sys.argv[1]).lower() != 'pre') and (str(sys.argv[1]).lower() != 'pro'):
+			print 'Este script obtiene el número de dataset y recursos haciendo uso de la api y genera un javascript que contiene su valor'
+			print 'Uso: python getCountData.py pre|pro'
+		else:
+			servidor=''
+			if (str(sys.argv[1]).lower() == 'pre'):
+				servidor='http://preopendata.aragon.es'
+			elif (str(sys.argv[1]).lower() == 'pro'):
+				servidor='http://opendata.aragon.es'
+			datos=obtenDatos('temporal.json', servidor)
+			generaJavascript(datos)
+	else:
+		print 'Este script obtiene el número de dataset y recursos haciendo uso de la api y genera un javascript que contiene su valor'
+		print 'Uso: python getCountData.py pre|pro'
+
+main()
